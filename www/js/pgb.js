f04bdf695e807931c2987b6f752c910d9cdd99de
Paweł Rachwał <Pawel@Air-Pawe.home>
2017-04-12 20:27:09 +0200

    end

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -9,6 +9,20 @@ function onDeviceReady() {
 
 function random() {
 	var number = Math.floor((Math.random() * 3) + 1);
-	document.getElementById("randomize").innerHTML = "<img class=\"popphoto\" src=\"./img/test" + number + ".png\" style=\"max-height:512px;\">;"	
+	document.getElementById("randomize").innerHTML = "<img src=\"./img/test" + number + ".png\">";
+}
+
+function kiss() {
+
+	var arr = []
+	while(arr.length < 3){
+    var randomnumber = Math.ceil(Math.random()*3)
+    if(arr.indexOf(randomnumber) > -1) continue;
+    arr[arr.length] = randomnumber;
+}
+	
+	document.getElementById("one").innerHTML = "<img src=\"./img/test" + arr[0] + ".png\">";
+	document.getElementById("two").innerHTML = "<img src=\"./img/test" + arr[1] + ".png\">";
+	document.getElementById("three").innerHTML = "<img src=\"./img/test" + arr[2] + ".png\">";
 }
 
